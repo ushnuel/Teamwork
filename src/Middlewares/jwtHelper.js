e98b6c842ec638ad968463dc46af26aa7136b530
Emmanuel <ezeemmanuel2010@gmail.com>
2019-10-31 17:48:37 +0100

    feature(signin): employee and admin can sign in
    [Start #515418134]

--- src/Middlewares/jwtHelper.js
+++ src/Middlewares/jwtHelper.js
@@ -1,8 +1,37 @@
 import jwt from 'jsonwebtoken';
 import config from '../Config';
+import ErrorHandler from '../Helpers';
 
 export default class JWT {
   static createToken(payload) {
     return jwt.sign(payload, config.JWT_SECRET);
+  }
+
+  static authorize(req, res, next) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      if (token) {
+        jwt.verify(token, config.JWT_SECRET, (err, decoded) => {
+          if (err) {
+            throw new ErrorHandler('Authentication failed: Invalid token', 401);
+          }
+          req.user = decoded;
+        });
+        next();
+      } else {
+        throw new ErrorHandler('Authentication failed: No token provided', 401);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static authorizeAdmin(req, res, next) {
+    const { status } = req.user;
+    if (status !== 'admin') {
+      next(new ErrorHandler('Unauthorized access', 403));
+    } else {
+      next();
+    }
   }
 }
--- src/Tests/employee.test.js
+++ src/Tests/employee.test.js
@@ -7,7 +7,7 @@ import Employee from './Mockups/employee';
 
 chai.use(chaiHttp);
 should();
-const { employee } = new Employee();
+const { employee, validEmployee, invalidEmployee } = new Employee();
 const route = '/api/v1/auth';
 
 describe('EMPLOYEE AUTHENTICATION TEST', () => {
@@ -28,6 +28,39 @@ describe('EMPLOYEE AUTHENTICATION TEST', () => {
 
           const { message } = data;
           expect(message).to.be.eql('User account successfully created');
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  describe('Sign in feature', () => {
+    it('Admin/Employee can sign in', (done) => {
+      chai
+        .request(server)
+        .post(`${route}/signin`)
+        .send(validEmployee)
+        .then((res) => {
+          res.should.be.an('object');
+          const { data, status } = res.body;
+          expect(status).to.eql('success');
+          expect(data).to.have.property('token');
+          expect(data).to.have.property('userId');
+
+          done();
+        })
+        .catch(err => done(err));
+    });
+
+    it('Throw Error for an invalid user', (done) => {
+      chai
+        .request(server)
+        .post(`${route}/signin`)
+        .send(invalidEmployee)
+        .then((res) => {
+          const { status } = res.body;
+          expect(res).to.have.property('error');
+          expect(status).to.eql('error');
           done();
         })
         .catch(err => done(err));
