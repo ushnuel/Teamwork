6f5fa6a08a1ba25dfed1b4bbcb7050ab6a8c74d0
Emmanuel <ezeemmanuel2010@gmail.com>
2019-11-04 18:09:53 +0100

    feature(gif): employees delete their gif posts
    [Fixes #14]

--- src/Tests/gif.test.js
+++ src/Tests/gif.test.js
@@ -13,6 +13,7 @@ const { gifPost, post } = new Gif();
 const { employee } = new Employee();
 const route = '/api/v1';
 const newEmployee = {};
+const newGif = {};
 
 describe('GIF TESTS', () => {
   before((done) => {
@@ -22,6 +23,7 @@ describe('GIF TESTS', () => {
       .send(employee)
       .then((res) => {
         const { data } = res.body;
+        newEmployee.id = data.userId;
         newEmployee.token = data.token;
         done();
       })
@@ -31,21 +33,23 @@ describe('GIF TESTS', () => {
   describe('POST /gifs', () => {
     it('Employees can create and share gif with colleaques', (done) => {
       const file = fs.readFileSync(gifPost.imagePath);
+      post.employeeID = newEmployee.id;
       chai
         .request(server)
         .post(`${route}/gifs`)
         .auth(newEmployee.token, { type: 'bearer' })
         .set('Content-Type', 'application/x-www-form-urlencoded')
         .field('title', post.title)
+        .field('employeeID', post.employeeID)
         .attach('image', file, 'giphy.gif')
         .then((res) => {
           const { data } = res.body;
           expect(data).have.property('image_url');
           expect(data).to.have.property('gifid');
           expect(data).to.have.property('createdon');
-
           const { message } = data;
           expect(message).to.eql('GIF image successfully posted');
+          newGif.id = data.gifid;
           done();
         })
         .catch(err => done(err));
@@ -53,12 +57,14 @@ describe('GIF TESTS', () => {
 
     it('throw error if image is not in gif format', (done) => {
       const file = fs.readFileSync(gifPost.incorrectImagePath);
+      post.employeeID = newEmployee.id;
       chai
         .request(server)
         .post(`${route}/gifs`)
         .auth(newEmployee.token, { type: 'bearer' })
         .set('Content-Type', 'application/x-www-form-urlencoded')
         .field('title', post.title)
+        .field('employeeID', post.employeeID)
         .attach('image', file, 'jpeg.jpg')
         .then((res) => {
           expect(res.body).have.property('error');
@@ -67,6 +73,35 @@ describe('GIF TESTS', () => {
           expect(error).to.eql(
             'Invalid image format: Image should be in a gif format',
           );
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  describe('DELETE /gifs/:gifId', () => {
+    it('Employees can delete their gif posts', (done) => {
+      chai
+        .request(server)
+        .delete(`${route}/gifs/${newGif.id}`)
+        .auth(newEmployee.token, { type: 'bearer' })
+        .then((res) => {
+          const { message } = res.body.data;
+          expect(message).to.eql('Gif post successfully deleted');
+          done();
+        })
+        .catch(err => done(err));
+    });
+
+    it('Throw error if gif does not exist', (done) => {
+      chai
+        .request(server)
+        .delete(`${route}/gifs/${newGif.id + 1}`)
+        .auth(newEmployee.token, { type: 'bearer' })
+        .then((res) => {
+          expect(res.body).to.have.property('error');
+          const { error } = res.body;
+          expect(error).to.eql('Gif not found, cannot process request');
           done();
         })
         .catch(err => done(err));
