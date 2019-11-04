245b929892690c947179355f21bf51bb747ad1d3
Emmanuel <ezeemmanuel2010@gmail.com>
2019-11-04 13:00:22 +0100

    feature(delete): employees can delete their articles
    [Fixes #10]

--- src/Routes/article.js
+++ src/Routes/article.js
@@ -4,5 +4,6 @@ import ArticleController from '../Controllers/article';
 const router = Router();
 router.post('/', ArticleController.create);
 router.patch('/:articleId', ArticleController.edit);
+router.delete('/:articleId', ArticleController.delete);
 
 export default router;
