9bee430090485c750c5e6c953683d34f40aa3f9b
Emmanuel <ezeemmanuel2010@gmail.com>
2019-11-10 14:01:17 +0100

    chore(swagger): document api endpoints
    [Fixes #44]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-/* eslint-disable no-console */
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
 import '@babel/polyfill';
 import express, { json, urlencoded } from 'express';
 import cors from 'cors';
-
+import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 import Routes from './Routes';
 import config from './Config';
-import { FeedbackHandler } from './Helpers';
+import { FeedbackHandler, ErrorHandler } from './Helpers';
+
+const debug = require('debug')('http');
 
 const app = express();
 
@@ -21,13 +26,25 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(morgan('dev'));
 app.use('/api/v1', Routes);
+
+app.get('/', (req, res) => res.json({
+    message: 'Welcome to Warefarer server API homepage',
+  }),);
+
+app.use('*', (req, res, next) => {
+  const error = new ErrorHandler('Page Not Found', 404);
+  next(error);
+});
+
 app.use(FeedbackHandler.error);
 
 app.set('port', config.PORT);
 
 app.listen(config.PORT, () => {
-  console.log('Server started on port', config.PORT);
+  debug('Server started on port', config.PORT);
 });
 
 export default app;
